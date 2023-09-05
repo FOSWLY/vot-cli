546d452d027f5df46fb49a53ef56c74ebec39527
unknown <toil.official@yandex.com>
2023-09-05 22:47:34 +0300

    added supported langs

---
+++ src/config/constants.js
@@ -0,0 +1,36 @@
+// available languages for translation
+const availableLangs = [
+  "ru",
+  "en",
+  "zh",
+  "ko",
+  "ar",
+  "fr",
+  "it",
+  "es",
+  "de",
+  "ja",
+];
+
+// Additional languages working with TTS
+const additionalTTS = [
+  "bn",
+  "pt",
+  "cs",
+  "hi",
+  "mr",
+  "te",
+  "tr",
+  "ms",
+  "vi",
+  "ta",
+  "jv",
+  "ur",
+  "fa",
+  "gu",
+  "id",
+  "uk",
+  "kk",
+];
+
+export { availableLangs, additionalTTS };
