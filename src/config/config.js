1467827013b3e7fe5cf4ec678008caf91fd7a2b3
unknown <toil.official@yandex.com>
2023-09-05 22:47:16 +0300

    added default config. Cloudflare worker doesn't supported

---
+++ src/config/config.js
@@ -0,0 +1,8 @@
+const debug = false;
+const workerHost = "api.browser.yandex.ru";
+
+const yandexHmacKey = "xtGCyGdTY2Jy6OMEKdTuXev3Twhkamgm";
+const yandexUserAgent =
+  "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 YaBrowser/23.7.1.1140 Yowser/2.5 Safari/537.36";
+
+export { debug, workerHost, yandexHmacKey, yandexUserAgent };
