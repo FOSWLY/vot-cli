70b66cf06abbb6fa599e92fd8968bcbbf2b6e7d9
unknown <toil.official@yandex.com>
2023-11-16 03:18:11 +0300

    added subtitles support

--- src/index.js
+++ src/index.js
@@ -10,10 +10,12 @@ import { availableLangs, additionalTTS } from "./config/constants.js";
 import validate from "./utils/validator.js";
 import getVideoId from "./utils/getVideoId.js";
 import translateVideo from "./translateVideo.js";
-import logger from "./utils/logger.js";
 import downloadFile from "./download.js";
+import { clearFileName } from "./utils/utils.js";
+import yandexRequests from "./yandexRequests.js";
+import yandexProtobuf from "./yandexProtobuf.js";
 
-const version = "1.1.1";
+const version = "1.2.0";
 const HELP_MESSAGE = `
 A small script that allows you to download an audio translation from Yandex via the terminal.
 
@@ -28,6 +30,7 @@ Args:
 Options:
   -h, --help — Show help
   -v, --version — Show version
+  --subs, --subtitles — Get video subtitles instead of audio (the subtitle language for saving is taken from --reslang)
 `;
 
 // LANG PAIR
@@ -39,6 +42,7 @@ const argv = parseArgs(process.argv.slice(2));
 
 const ARG_LINKS = argv._;
 const OUTPUT_DIR = argv.output;
+const IS_SUBS_REQ = argv.subs || argv.subtitles;
 const ARG_HELP = argv.help || argv.h;
 const ARG_VERSION = argv.version || argv.v;
 
@@ -47,7 +51,10 @@ if (availableLangs.includes(argv.lang)) {
   console.log(`Request language is set to ${REQUEST_LANG}`);
 }
 
-if ([...availableLangs, ...additionalTTS].includes(argv.reslang)) {
+if (
+  additionalTTS.includes(argv.reslang) ||
+  (Boolean(IS_SUBS_REQ) && argv.reslang)
+) {
   RESPONSE_LANG = argv.reslang;
   console.log(`Response language is set to ${RESPONSE_LANG}`);
 }
@@ -59,7 +66,7 @@ const tasks = new Listr([], {
 });
 
 const translate = async (finalURL, task) => {
-  let test;
+  let translateData;
 
   try {
     await translateVideo(
@@ -76,9 +83,8 @@ const translate = async (finalURL, task) => {
           }
 
           task.title = "Video translated successfully.";
-          console.info(`Audio Link (${finalURL}): "${chalk.gray(urlOrError)}"`)
-          // console.log("TEST", success, urlOrError)
-          test = {
+          console.info(`Audio Link (${finalURL}): "${chalk.gray(urlOrError)}"`);
+          translateData = {
             success,
             urlOrError,
           };
@@ -100,7 +106,76 @@ const translate = async (finalURL, task) => {
     };
   }
 
-  return test;
+  return translateData;
+};
+
+const fetchSubtitles = async (finalURL, task) => {
+  let subtitlesData;
+
+  try {
+    await yandexRequests.requestVideoSubtitles(
+      finalURL,
+      REQUEST_LANG,
+      (success, response) => {
+        if (!success) {
+          throw new Error(chalk.red("Failed to get Yandex subtitles"));
+        }
+
+        const subtitlesResponse =
+          yandexProtobuf.decodeSubtitlesResponse(response);
+
+        let subtitles = subtitlesResponse.subtitles ?? [];
+        subtitles = subtitles.reduce((result, yaSubtitlesObject) => {
+          if (
+            yaSubtitlesObject.language &&
+            !result.find((e) => {
+              if (
+                e.source === "yandex" &&
+                e.language === yaSubtitlesObject.language &&
+                !e.translatedFromLanguage
+              ) {
+                return e;
+              }
+            })
+          ) {
+            result.push({
+              source: "yandex",
+              language: yaSubtitlesObject.language,
+              url: yaSubtitlesObject.url,
+            });
+          }
+          if (yaSubtitlesObject.translatedLanguage) {
+            result.push({
+              source: "yandex",
+              language: yaSubtitlesObject.translatedLanguage,
+              translatedFromLanguage: yaSubtitlesObject.language,
+              url: yaSubtitlesObject.translatedUrl,
+            });
+          }
+          return result;
+        }, []);
+
+        task.title = "Subtitles for the video have been received.";
+        console.info(
+          `Subtitles response (${finalURL}): "${chalk.gray(
+            JSON.stringify(subtitles, null, 2),
+          )}"`,
+        );
+
+        subtitlesData = {
+          success: true,
+          subsOrError: subtitles,
+        };
+      },
+    );
+  } catch (e) {
+    return {
+      success: false,
+      subsOrError: e.message,
+    };
+  }
+
+  return subtitlesData;
 };
 
 async function main() {
@@ -153,6 +228,7 @@ async function main() {
               },
               {
                 title: `Translating (ID: ${videoId}).`,
+                enabled: !IS_SUBS_REQ,
                 exitOnError: false,
                 task: async (ctxSub, subtask) => {
                   // ! TODO: НЕ РАБОТАЕТ ЕСЛИ ВИДЕО НЕ ИМЕЕТ ПЕРЕВОДА
@@ -184,15 +260,35 @@ async function main() {
                       reject(e);
                     }
                   });
-                  // console.log("RESULT", res)
+                },
+              },
+              {
+                title: `Fetching subtitles (ID: ${videoId}).`,
+                enabled: Boolean(IS_SUBS_REQ),
+                exitOnError: false,
+                task: async (ctxSub, subtask) => {
+                  await new Promise(async (resolve, reject) => {
+                    try {
+                      let result;
+                      result = await fetchSubtitles(parent.finalURL, subtask);
+                      parent.translateResult = result;
+                      if (!result.success) {
+                        subtask.title = result.urlOrError;
+                      }
+                      resolve(result);
+                    } catch (e) {
+                      reject(e);
+                    }
+                  });
                 },
               },
               {
                 title: `Downloading (ID: ${videoId}).`,
                 exitOnError: false,
-                enabled: Boolean(OUTPUT_DIR),
+                enabled: Boolean(OUTPUT_DIR) && !IS_SUBS_REQ,
                 task: async (ctxSub, subtask) => {
-                  // await sleep(5000)
+                  // * Video download
+
                   if (
                     !(
                       parent.translateResult?.success &&
@@ -207,11 +303,63 @@ async function main() {
                   }
 
                   const taskSubTitle = `(ID: ${videoId})`;
+                  const filename = `${clearFileName(
+                    videoId,
+                  )}---${uuidv4()}.mp3`;
                   await downloadFile(
                     parent.translateResult.urlOrError,
-                    `${OUTPUT_DIR}/${videoId}---${uuidv4()}.mp3`,
+                    `${OUTPUT_DIR}/${filename}`,
                     subtask,
-                    `(ID: ${videoId})`,
+                    `(ID: ${videoId} as ${filename})`,
+                  )
+                    .then(() => {
+                      subtask.title = `Download ${taskSubTitle} completed!`;
+                    })
+                    .catch((e) => {
+                      subtask.title = `Error. Download ${taskSubTitle} failed! Reason: ${e.message}`;
+                    });
+                },
+              },
+              {
+                title: `Downloading (ID: ${videoId}).`,
+                exitOnError: false,
+                enabled: Boolean(OUTPUT_DIR) && Boolean(IS_SUBS_REQ),
+                task: async (ctxSub, subtask) => {
+                  // * Subs download
+
+                  if (
+                    !(
+                      parent.translateResult?.success &&
+                      parent.translateResult?.subsOrError
+                    )
+                  ) {
+                    throw new Error(
+                      chalk.red(
+                        `Downloading failed! Link "${parent.translateResult?.subsOrError}" not found`,
+                      ),
+                    );
+                  }
+
+                  const subOnReqLang = parent.translateResult.subsOrError.find(
+                    (s) => s.language === RESPONSE_LANG,
+                  );
+                  if (!subOnReqLang) {
+                    throw new Error(
+                      chalk.red(
+                        `Downloading failed! Failed to find ${RESPONSE_LANG} in the resulting list of subtitles`,
+                      ),
+                    );
+                  }
+
+                  const taskSubTitle = `(ID: ${videoId})`;
+                  const filename = `${subOnReqLang.language}---${clearFileName(
+                    videoId,
+                  )}---${uuidv4()}.json`;
+                  await downloadFile(
+                    subOnReqLang.url,
+                    `${OUTPUT_DIR}/${filename}`,
+                    subtask,
+                    `(ID: ${videoId} as ${filename})`,
                   )
                     .then(() => {
                       subtask.title = `Download ${taskSubTitle} completed!`;
